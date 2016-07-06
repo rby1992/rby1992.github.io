567c43c47d7fbc4eb46346d7711e203d33e8c02a
rby1992 <1035095626@qq.com>
2016-07-06 12:45:59 +0800

    2016/7/6

---
+++ js/data.js
@@ -0,0 +1,182 @@
+var aData = {
+	p2list : [
+		{
+			hd : '任务协作',
+			content : '随时记录需要做的事情或备忘想法，定期安排哪一天进行，及时与其他同事共同协作并与上下级指导沟通。',
+			hf : 'http://www.eteams.cn/product/task',
+			cl : 'p2_task'
+		},
+		{
+			hd : '项目',
+			content : '围绕着个人或团队的周期项目，以项目协作的方式及时做好工作计划、并汇总执行过程中的进展。',
+			hf : 'http://www.eteams.cn/product/goal',
+			cl : 'p2_goal'
+		},
+		{
+			hd : '工作报告',
+			content : '每周、每月、每年达成了哪些工作成效、总结成长的心得，并简明扼要的汇报下一步的总体计划。',
+			hf : 'http://www.eteams.cn/product/report',
+			cl : 'p2_report'
+		},
+		{
+			hd : '文档库',
+			content : '建立团队统一的知识网盘，积累沉淀工作过程中的知识经验，全员创新、全员分享、充分利用。',
+			hf : 'http://www.eteams.cn/product/doc',
+			cl : 'p2_doc'
+		},
+		{
+			hd : '客户库',
+			content : '客户、供应商等所有外部资源信息统一管理起来，跟踪客户的每一步过程内容及下一步联络计划。',
+			hf : 'http://www.eteams.cn/product/crm',
+			cl : 'p2_crm'
+		},
+		{
+			hd : '日报分享',
+			content : '记录每一天的工作成果和经验分享，可透视整个组织每位成员的工作状态和成效，洞察每件事项的进度。',
+			hf : 'http://www.eteams.cn/product/blog',
+			cl : 'p2_blog'
+		},
+		{
+			hd : '企业微信',
+			content : '团队内部的微信，每一次沟通都是围绕着工作成果及其事项协作建立针对性的互动。',
+			hf : 'http://www.eteams.cn/product/chat',
+			cl : 'p2_chat'
+		},
+		{
+			hd : '移动考勤',
+			content : '基于地理位置的手机签到签退并结合企业上下班时间，自动形成团队成员的工作考勤轨迹。',
+			hf : 'http://www.eteams.cn/product/timecard',
+			cl : 'p2_timecard'
+		},
+		{
+			hd : '流程审批',
+			content : '各行各业的业务表单库帮助您快速借鉴同类公司、同等规模的企业管理经验和内部规范要求。',
+			hf : 'http://www.eteams.cn/product/flow',
+			cl : 'p2_flow'
+		}
+	],
+	p3list : [
+		{
+			hd : '移动APP',
+			cl : 'p3_app',
+			content : '一款智能手机，就可轻松地将您团队的任务、日报、文档、客户、流程等全部事项放进您的eteams中。'
+		},
+		{
+			hd : '微信企业号',
+			cl : 'p3_qyh',
+			content : '无需安装移动端，基于企业号的云OA办公，不漏掉任何一条与工作相关的消息。'
+		},
+		{
+			hd : '微信服务号',
+			cl : 'p3_weixin',
+			content : '无需注册企业号，只要关注eteams服务号就可以马上内部工作协作。'
+		},
+		{
+			hf : 'http://www.eteams.cn/mobile',
+			cl : 'p3_more',
+			content : '了解更多'
+		}
+	],
+	p6list : [
+		{
+			tm : '2016-03-11',
+			hf : 'http://www.eteams.cn/blog/4525768707336968375',
+			content : '你还在痴迷大数据？人家早就依靠小数据掘金教育行业获千万收益了'
+		},
+		{
+			tm : '2016-03-07',
+			hf : 'http://www.eteams.cn/blog/4525734471776645735',
+			content : '女神节要到了，看看别人家公司都是怎么玩的！'
+		},
+		{
+			tm : '2016-01-20',
+			hf : 'http://www.eteams.cn/blog/4525328211446350067',
+			content : '传统五金企业重启项目管理模式，变传统为不同'
+		},
+		{
+			tm : '2016-01-18',
+			hf : 'http://www.eteams.cn/blog/4525310832590050057',
+			content : '用户说：广告初创企业想要客户，首先得学会管理客户！'
+		},
+		{
+			tm : '2016-01-07',
+			hf : 'http://www.eteams.cn/blog/4525214883263542573',
+			content : '“晒工作成绩单 赢iPhone6s”活动攻略'
+		},
+		{
+			tm : '2015-12-17',
+			hf : 'http://www.eteams.cn/blog/4525033209363075289',
+			content : '微信运营也要看技术 '
+		},
+		{
+			tm : '2015-12-17',
+			hf : 'http://www.eteams.cn/blog/4525033124322775288',
+			content : '只是一张表，治好了我的尴尬症！ '
+		},
+		{
+			tm : '2016-06-08',
+			hf : 'http://www.eteams.cn/blog/4526536827349545459',
+			content : '职场逆袭不靠手段，而靠这些习惯…… '
+		},
+		{
+			tm : '2016-06-08',
+			hf : 'http://www.eteams.cn/blog/4526536772224945458',
+			content : '想做好销售，得先用好工具！ '
+		},
+		{
+			tm : '2016-05-27',
+			hf : 'http://www.eteams.cn/blog/4526432044415368371',
+			content : '老板一拍脑袋，7天后开发布会，我是如何搞定一切的？'
+		},
+		{
+			tm : '2016-05-27',
+			hf : 'http://www.eteams.cn/blog/4526431604848168370',
+			content : '他们就是这样做到了店铺零差评…… '
+		},
+		{
+			tm : '2016-05-25',
+			hf : 'http://www.eteams.cn/blog/4526417138873568365',
+			content : '做表这件事没点套路怎么行？'
+		},
+		{
+			tm : '2016-05-25',
+			hf : 'http://www.eteams.cn/blog/4526417107479168364',
+			content : '他们就是这样把公司做到2015年全球智能硬件TOP5的……'
+		},
+		{
+			tm : '2016-05-09',
+			hf : 'http://www.eteams.cn/blog/4526279120967868345',
+			content : '不刷脸、不刷卡，考勤早有新方法 '
+		},
+		{
+			tm : '2016-04-28',
+			hf : 'http://www.eteams.cn/blog/4526181408954778297',
+			content : '不玩转内部管理，怎么玩转资本…… '
+		},
+		{
+			tm : '2016-04-21',
+			hf : 'http://www.eteams.cn/blog/4526122529290430629',
+			content : '这家公司造房子就像开了挂！ '
+		},
+		{
+			tm : '2016-03-24',
+			hf : 'http://www.eteams.cn/blog/4525881535910545774',
+			content : '淘汰你的从来只是工具，而不是年龄 '
+		},
+		{
+			tm : '2016-03-24',
+			hf : 'http://www.eteams.cn/blog/4525881492797245773',
+			content : '好东西要分享，创意也一样'
+		},
+		{
+			tm : '2016-03-16',
+			hf : 'http://www.eteams.cn/blog/4525812380577549422',
+			content : '创业团队，到底要不要加班？'
+		},
+		{
+			tm : '2016-03-16',
+			hf : 'http://www.eteams.cn/blog/4525812294158849421',
+			content : '招聘季，招人难，难倒你们了吗？ '
+		}
+	]
+}
---
+++ js/new_file.js
@@ -0,0 +1,424 @@
+window.onload = function() {
+	//console.log(window.innerHeight);
+	var warp = document.getElementById('warp');
+	var p = warp.children;
+	var movea = $('.movenav span');
+	var num = 0;
+	var onoff = true;
+	var iH = window.innerHeight;
+	var iW = window.innerWidth;
+	var head = document.getElementById('HEAD');
+	var p2ls = aData.p2list;
+	var p3ls = aData.p3list;
+	var p6ls = aData.p6list;
+	//console.log(aData.p3list);
+	var p2lis = document.querySelectorAll('.p2lis')[0];
+	var p3lis = document.getElementById('p3Conw');
+	var p6lis = document.querySelectorAll('.p6ul')[0];
+	//console.log(p6lis);
+	var q = 0;
+	var p4Cll = document.querySelectorAll('.p4Cll');
+	var p4Clr = document.querySelectorAll('.p4Clr');
+	var p4CllM = document.querySelectorAll('.p4CllM');
+	var p4ClrM = document.querySelectorAll('.p4ClrM');
+	var timerp3 = null;
+	var p5img = document.querySelectorAll('.p5img');
+	var p5h2 = document.querySelectorAll('.p5h2');
+	var p6btn = document.querySelectorAll('.btn');
+	var p6ul = document.querySelectorAll('.p6ul')[0];
+	//console.log(p4Cll.currentStyle);
+	//console.log(p4Cll)
+	var moveul = document.querySelectorAll('.moveul li a');
+	var atop = document.getElementById('a_top');
+	var fxdiv = document.querySelectorAll('.fixeddiv')[0];
+	var heada = document.querySelectorAll('.nav a');
+	var zc = document.getElementById('Zc');
+	p6btn[5].style.display = "none";
+	var p1down = document.getElementById('p1Down');
+	p1down.onclick = function(){
+		num = 1;
+		movep();
+	}
+	atop.onclick = function(){
+		//console.log(-num * iH + warp.offsetTop + 'px')
+		 var timer = setInterval(function(){
+			//var scT = fxdiv.offsetTop;
+			var bktop = num * iH + warp.offsetTop + 'px';
+			warp.style.top = bktop;
+
+			console.log(warp.style.top)
+			if(warp.style.top == bktop){//console.log(1)
+				num = 0;
+				for(var i = 0; i < movea.length; i++){
+					movea[num].style.backgroundColor = 'transparent';
+					movea[i].style.backgroundColor = 'rgb(255, 255, 255)';
+					movea[i].className = '';
+					
+				};
+				movep();
+				movea[num].className = 'active';
+				clearInterval(timer);
+			}
+		},30);
+	}
+	for(var i = 0; i < moveul.length; i++){
+		moveul[i].index = i;
+		moveul[i].onclick = function(){
+			num = this.index;
+			movep();
+		};
+	};
+	function movep(){
+		if(num == 0){
+			p6btn[5].style.display = "none";
+			head.className = 'hd1';
+		}else{
+			p6btn[5].style.display = "block";
+		}
+		if(num == 1){
+			setTimeout(function(){
+				$('#p2Conw')[0].style.opacity = '1';
+				$('#p2Conw')[0].style.top = '0px';
+				$('.p2Ch2')[0].style.opacity = '1';
+				$('.p2Ch2')[0].style.top = '0px';
+			},1000)		
+		}else{				
+			$('#p2Conw')[0].style.opacity = '0';
+			$('#p2Conw')[0].style.top = '50px';
+			$('.p2Ch2')[0].style.opacity = '0';
+			$('.p2Ch2')[0].style.top = '40px';						
+		}
+		if(num == 2 ){
+			head.className = 'hd3';
+			setTimeout(function(){
+				
+				$('.p3h2')[0].style.top = '50%';
+				$('.p3h2')[0].style.opacity = '1';
+				picover();
+				timerp3 = setInterval(function(){
+					//console.log($('.p3_ld')[q])
+					//console.log(1)
+					move($('.p3_ld')[q],{
+						left:0,							
+						opacity:1
+					},200,'easeOut');
+					q++;
+					//console.log(q)
+					if(q == $('.p3_ld').length){//console.log(1)
+						$('.p3_more')[0].style.marginTop = '20px';
+						$('.p3_more')[0].style.opacity = '1';
+						clearInterval(timerp3);
+					}
+				},500);
+			},600)
+		}else {
+			
+			clearInterval(timerp3);
+			picout();
+			$('.p3_more')[0].style.marginTop = '30px';
+			$('.p3_more')[0].style.opacity = '0';
+			$('.p3_more')[0].style.transition = '0.3s';
+			$('.p3h2')[0].style.transition = '0.3s';
+			$('.p3h2')[0].style.top = '45%';
+			$('.p3h2')[0].style.opacity = '0';
+			q = 0;
+			for(var i = 0; i < $('.p3_ld').length; i++){
+				//$('.p3_ld')[i].style.transition = i*0.7+1+'s';
+				$('.p3_ld')[i].style.left = '-100px';
+				$('.p3_ld')[i].style.opacity = '0';
+			}
+		}
+		if(num == 3){
+			setTimeout(function(){
+				
+				//$('.p4Cll').each(function(i,elem){})
+					move(p4Cll[0],{
+						left:0,							
+						opacity:1
+					},600,'linear',function(){
+						move(p4CllM[0],{
+							right:14,							
+							opacity:1
+						},300,'linear')
+					});
+					move(p4Cll[1],{
+						left:0,							
+						opacity:1
+					},600,'linear',function(){
+						move(p4CllM[1],{
+							right:14,							
+							opacity:1
+						},300,'linear')
+					});
+					move(p4Cll[2],{
+						left:0,							
+						opacity:1
+					},600,'linear',function(){
+						move(p4CllM[2],{
+							right:14,							
+							opacity:1
+						},300,'linear')
+					});
+					move(p4Clr[0],{
+						right:0,							
+						opacity:1
+					},600,'linear',function(){
+						move(p4ClrM[0],{
+							left:14,							
+							opacity:1
+						},300,'linear')
+					});
+					move(p4Clr[1],{
+						right:0,							
+						opacity:1
+					},600,'linear',function(){
+						move(p4ClrM[1],{
+							left:14,							
+							opacity:1
+						},300,'linear')
+					});
+					move(p4Clr[2],{
+						right:0,							
+						opacity:1
+					},600,'linear',function(){
+						move(p4ClrM[2],{
+							left:14,							
+							opacity:1
+						},300,'linear')
+					});
+				//}
+			},800)
+		}else{
+			
+			for(var i = 0; i < p4Cll.length; i++){
+				p4Cll[i].style.transition = '0.6s';
+				p4Cll[i].style.left = '-100px';
+				p4Cll[i].style.opacity = '0';
+				//p4CllM[i].style.transition = '0.5s';
+				p4CllM[i].style.right = '-41px';
+				p4CllM[i].style.opacity = '0';
+			};
+			for(var i = 0; i < p4Clr.length; i++){
+				p4Clr[i].style.transition = '0.6s';
+				p4Clr[i].style.right = '-100px';
+				p4Clr[i].style.opacity = '0';
+				//p4ClrM[i].style.transition = '0.5s';
+				p4ClrM[i].style.left = '-41px';
+				p4ClrM[i].style.opacity = '0';
+			}
+		}
+		if(num == 4){
+			head.className = 'hd5';
+			setTimeout(function(){
+				p5h2[0].style.marginTop = '-22px';
+				p5h2[0].style.opacity = '1';
+			},500)
+			setTimeout(function(){
+				p5img[0].style.transform = 'scale(1)';
+				p5img[0].style.opacity = '1';
+			},500)
+		}else{
+			p5h2[0].style.marginTop = '20px';
+			p5h2[0].style.opacity = '0';
+			p5img[0].style.transform = 'scale(3)';
+			p5img[0].style.opacity = '0';
+		}
+		if(num == 5){
+			head.className = 'hd6';
+		}
+		if(num == 1 || num == 3){
+			head.className = 'header';
+		}
+		if(num == 0 || num == 2 || num == 5){
+			movea.css('background','#fff');
+		}else{
+			movea.css('background','#ccc');
+		}
+		if(num == 2 || num == 4 || num == 5){
+			zc.style.color = "#fff";
+			for(var i = 0; i < heada.length; i++){
+				heada[i].style.color = '#fff';
+			}
+		}else{
+			zc.style.color = "#5c5c5c";
+			for(var i = 0; i < heada.length; i++){
+				heada[i].style.color = '#5c5c5c';
+			}
+		}
+		for(var i = 0; i < movea.length; i++){
+			movea[i].className = '';
+			movea[num].style.backgroundColor = 'transparent';
+		}
+		movea[num].className = 'active';
+		warp.style.top = -num * iH + 'px';
+	}
+	for(var i = 0; i < p.length; i++){//console.log(warp);
+		p[i].style.height = iH + 'px';
+		p[i].style.width = iW + 'px';
+	}
+	movea[0].className = 'active';
+	movea[0].style.backgroundColor = 'transparent';
+	movea[0].style.borderColor = '#fff';
+	console.log(p2ls.length)
+	for(var i = 0; i < p2ls.length; i++){
+		p2lis.innerHTML += '<a href = "'+p2ls[i].hf+'" class = "p2la">'+
+		'<div class ="p2abg '+p2ls[i].cl+'"></div>'+
+		'<h4>'+p2ls[i].hd+'</h4>'+
+		'<p>'+p2ls[i].content+'</p></a>'	
+	};
+	for(var i = 0; i < p3ls.length-1; i++){
+		p3lis.innerHTML += '<div class = "p3_ld">'+
+		'<div class = "p3_img '+p3ls[i].cl+'"></div>'+
+		'<h4>'+p3ls[i].hd+'</h4>'+
+		'<p>'+p3ls[i].content+'</p></div>'		
+	};
+	p3lis.innerHTML += '<a href="'+p3ls[3].hf+'" class = "'+p3ls[3].cl+' hovermore">'+p3ls[3].content+'</a>';
+	var p3ld = $('.p3_ld')[0];
+	var arrbp1 = ['0 0','-42px 0','-84px 0'];
+	var arrbp2 = ['0 -42px','-42px -42px','-84px -42px'];
+	$('.p3_ld')[0].children[0].style.backgroundPosition = arrbp2[0];
+	$('.p3_ld')[0].children[1].style.color = '#00a0e9';
+	$('.p3pic')[0].className = 'p3pic p3bok';
+	$('.p3pic')[0].style.opacity = '1';
+	for(var i = 0; i < p6ls.length; i++){
+		p6lis.innerHTML += '<li><span class="p6lit">'+p6ls[i].tm+'</span>'+
+		'<a href="'+p6ls[i].hf+'">'+p6ls[i].content+'</a></li>'
+	}
+	for(var i = 1; i < p6btn.length-1; i++){
+		p6btn[i].onmouseover = function(){
+			this.children[1].style.display = 'block';
+		};
+		p6btn[i].onmouseout = function(){
+			this.children[1].style.display = 'none';
+		};
+	}
+	function picover(){
+		for(var i = 0; i < $('.pic1').length; i++){
+			$('.pic1')[i].style.right = '130px';
+			$('.pic1')[i].style.opacity = '1';
+		};
+		for(var i = 0; i < $('.pic2').length; i++){
+			$('.pic2')[i].style.right = '0px';
+			$('.pic2')[i].style.opacity = '1';
+		};
+	};
+	function picout(){
+		for(var i = 0; i < $('.pic1').length; i++){
+				$('.pic1')[i].style.right = '0px';
+				$('.pic1')[i].style.opacity = '0';
+				$('.pic1')[i].style.transition = '0.5s';
+			}
+			for(var i = 0; i < $('.pic2').length; i++){
+				$('.pic2')[i].style.right = '130px';
+				$('.pic2')[i].style.opacity = '0';
+				$('.pic2')[i].style.transition = '0.5s';
+			}
+	}
+//	for(var i = 0; i < p4CllM.length; i++){
+//		p4CllM[i].onmouseover = function(){
+//			this.style.backgroundPosition = 
+//		}
+//	}
+	var onoff2 = true;
+	for(var i = 0; i < $('.p3_ld').length; i++){
+		$('.p3_ld')[i].index = i;		
+		$('.p3_ld')[i].onmouseover = function(){
+			console.log(1)
+//			if(onoff2){
+//				onoff2 = false;
+				for(var i = 0; i < $('.p3pic').length; i++){//console.log($('.p3pic')[i])
+					//$('.p3pic')[i].className = 'p3pic p3none';
+					//$('.p3pic')[i].style.transition = '1s';
+					$('.p3pic')[i].style.opacity = '0';
+				};
+				//picover();
+				$('.p3pic')[this.index].className = 'p3pic p3bok';
+				var that = this;
+				setTimeout(function(){
+					$('.p3pic')[that.index].style.opacity = '1';
+				},200);
+	//			
+				//$($('.p3pic')[this.index]).siblings().css('display','none');
+				p3ld.children[0].style.backgroundPosition = arrbp1[p3ld.index];
+				p3ld.children[1].style.color = '#fff';
+				this.children[0].style.backgroundPosition = arrbp2[this.index];
+				this.children[1].style.color = '#00a0e9';
+				p3ld = this;
+			//};
+//			setTimeout(function(){
+//				onoff2 = true;
+//			},500)
+	};				
+//		$('.p3_ld')[i].onmouseout = function(){
+//			picout();
+//		}
+	};
+	for(var i = 0; i < $('.p3pic').length; i++){
+		$('.p3pic')[i].onmouseover = function(){
+			picover();
+		};
+		$('.p3pic')[i].onmouseout = function(){
+			for(var i = 0; i < $('.pic1').length; i++){
+				$('.pic1')[i].style.right = '0px';
+				$('.pic1')[i].style.opacity = '1';
+				$('.pic1')[i].style.transition = '0.5s';
+			}
+			for(var i = 0; i < $('.pic2').length; i++){
+				$('.pic2')[i].style.right = '130px';
+				$('.pic2')[i].style.opacity = '1';
+				$('.pic2')[i].style.transition = '0.5s';
+			}
+		};
+	};
+	var numT = 0;
+	p6ul.innerHTML+=p6ul.innerHTML;
+	var timer2 = setInterval(function(){
+		numT++;
+		
+		if(Math.abs(parseInt($(p6ul).css('marginTop'))) == $(p6ul).height()/2){
+			//console.log(1);
+			p6ul.style.marginTop = '0';
+			numT = 0;
+		}
+		
+//		if(numT > 200){
+//			
+//		}
+		$(p6ul).css({
+			marginTop:-numT
+		})
+//		move(p6ul,{
+//			marginTop : '-30'
+//		},1950,'linear',function(){//console.log(1)
+//			p6ul.style.marginTop = '0';
+//			p6ul.appendChild(p6ul.firstChild);
+//		});
+	},50);
+	
+	
+	
+	
+	addWhell(document,function(down){
+		if(onoff){
+			onoff = false;
+			if(down){
+				num --;
+				if(num <= 0){
+					num = 0;
+				}				
+			}else{
+				num++;
+				if(num == p.length){
+					num = p.length - 1;
+				}
+			}		
+			//console.log(num)
+			
+			movep();
+			setTimeout(function(){
+				onoff = true;
+			},1500);			
+		}
+	});
+	//console.log(num)
+}
